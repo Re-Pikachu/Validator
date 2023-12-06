e4256774f01f2436204abeea3e3dc5eb818927cf
Junglepanda69 <Conrady82@gmail.com>
2023-12-05 17:56:57 -0800

    create signin and home page

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import SignIn from "./components/SignIn";
+import Home from "./components/Home";
 //we didnt need to import {createRoot} individually since we imported in the whole module
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<App />);
-
